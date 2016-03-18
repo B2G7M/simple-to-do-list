6ab4856e982ba821314d5dcf4e4da3c554ebe714
Amos Marsters <amosmarsters@Amoss-MacBook-Pro.local>
2016-03-18 10:45:55 -0400

    this

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-var $input = $('#list-input');
-var $list = $('.toDoList');
-
-$('form').on('submit', function (e) {
-  var $li = $('<li>');
-
-  e.preventDefault();
-  $li.html($input.val());
-  $li.val('');
-  $list.append($li);
-
-});
-
-$('ul').on('click', 'li', function (f) {
-  $(this).toggleClass('crossOut');
-
-});
+// do you own code bro
